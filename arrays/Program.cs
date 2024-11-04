fceb9310e05319c3d14a023c17d0e370def17a04
Mahabubur Rahman <microronyy@gmail.com>
2024-11-04 19:16:47 +0600

    arrays challenge

---
+++ arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] fraudulentOrderIDs = {
+"B123",
+"C234",
+"A345",
+"C15",
+"B177",
+"G3003",
+"C235",
+"B179"};
+
+foreach (string ID in fraudulentOrderIDs)
+{
+  if (ID.StartsWith("B"))
+  {
+    Console.WriteLine(ID);
+  }
+}
