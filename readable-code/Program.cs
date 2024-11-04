c0552465174ab50e6c8237b8c1b831b5ef888678
Mahabubur Rahman <microronyy@gmail.com>
2024-11-04 20:26:41 +0600

    readable code

---
+++ readable-code/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+This code reverse a string and count how much a specific character appear's then prints the result in the console.
+*/
+
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+int letterCount = 0;
+
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
+
+foreach (char letter in message)
+{
+  if (letter == 'o')
+  {
+    letterCount++;
+  }
+}
+
+string newMessage = new String(message);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
