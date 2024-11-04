92646d4d00ae879e40eff2b37c47be593561559e
Mahabubur Rahman <microronyy@gmail.com>
2024-11-04 18:42:39 +0600

    if else challenge

---
+++ if-else-challenge/Program.cs
@@ -0,0 +1,34 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+string message = "";
+string discountMessage = "";
+
+if (daysUntilExpiration > 10) return;
+
+if (daysUntilExpiration <= 10 && daysUntilExpiration > 5)
+{
+  message = "Your subscription will expire soon. Renew now!";
+}
+else if (daysUntilExpiration <= 5 && daysUntilExpiration > 1)
+{
+  message = $"Your subscription expires in {daysUntilExpiration} days.";
+  discountPercentage = 10;
+}
+else if (daysUntilExpiration == 1)
+{
+  message = "Your subscription expires within a day!";
+  discountPercentage = 20;
+}
+else
+{
+  message = "Your subscription has expired.";
+}
+
+if (discountPercentage > 0)
+{
+  discountMessage = $"Renew now and save {discountPercentage}%.";
+}
+
+
+Console.WriteLine($"{message}\n{discountMessage}");
