71b46af7aea7d0a8865d51a256d8895dd41bd296
Mahabubur Rahman <microronyy@gmail.com>
2024-11-06 12:00:44 +0600

    first do while challenge

---
+++ DoWhileLoop/Program.cs
@@ -0,0 +1,28 @@
+﻿Random random = new Random();
+int heroHealthPoints = 10;
+int monsterHealthPoints = 10;
+string attacker = "hero";
+
+do
+{
+  int hitPoint = random.Next(1, 11);
+
+  switch (attacker)
+  {
+    case "hero":
+      monsterHealthPoints -= hitPoint;
+      Console.WriteLine($"Monster was damaged and lost {hitPoint} health and now has {monsterHealthPoints} health.");
+      attacker = "monster";
+      break;
+    case "monster":
+      heroHealthPoints -= hitPoint;
+      Console.WriteLine($"Hero was damaged and lost {hitPoint} health and now has {heroHealthPoints} health.");
+      attacker = "hero";
+      break;
+  }
+
+  if (heroHealthPoints > 0 && monsterHealthPoints > 0) continue;
+
+  Console.WriteLine(heroHealthPoints > 0 ? "Hero wins!" : "Monster wins!");
+
+} while (heroHealthPoints > 0 && monsterHealthPoints > 0);
