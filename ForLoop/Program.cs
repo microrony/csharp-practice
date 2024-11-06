86ef80779fba4890f5a6131ea954f1f4d3783c9d
Mahabubur Rahman <microronyy@gmail.com>
2024-11-06 10:31:51 +0600

    for loop

---
+++ ForLoop/Program.cs
@@ -0,0 +1,19 @@
+﻿for (int number = 1; number <= 100; number++)
+{
+  if (number % 3 == 0 && number % 5 == 0)
+  {
+    Console.WriteLine($"{number} - FizzBuzz");
+  }
+  else if (number % 5 == 0)
+  {
+    Console.WriteLine($"{number} - Buzz");
+  }
+  else if (number % 3 == 0)
+  {
+    Console.WriteLine($"{number} - Fizz");
+  }
+  else
+  {
+    Console.WriteLine(number);
+  }
+}
