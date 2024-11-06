7ba2ae9258dc310fd37cac9239abd79758b97201
Mahabubur Rahman <microronyy@gmail.com>
2024-11-06 15:10:14 +0600

    second do while and while loops challenge

---
+++ DoWhileWhileLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿bool validRole = false;
+string[] roles = new string[] { "administrator", "manager", "user" };
+
+Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+
+do
+{
+  string? userInput = Console.ReadLine();
+
+  if (userInput != null)
+  {
+    validRole = roles.Contains(userInput.Trim().ToLower());
+
+    if (validRole)
+    {
+      Console.WriteLine($"Your input value ({userInput.Trim()}) has been accepted.");
+    }
+    else
+    {
+      Console.WriteLine($"The role name that you entered, \"{userInput.Trim()}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+    }
+  }
+} while (validRole == false);
