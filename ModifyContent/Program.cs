2d54aaddd03b0b23daf431a1defbfdb82b127faa
Mahabubur Rahman <microronyy@gmail.com>
2024-11-08 19:46:40 +0600

    modify content exercise

--- ModifyContent/Program.cs
+++ ModifyContent/Program.cs
@@ -1,14 +1,33 @@
-﻿string message = "(What if) there are (more than) one (set of parentheses)?";
+﻿string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
 
+char[] openSymbols = { '(', '{', '[' };
+
+int closingPosition = 0;
 while (true)
 {
-  int openingPosition = message.IndexOf('(');
+  int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
   if (openingPosition == -1) break;
 
+  string currentSymbol = message.Substring(openingPosition, 1);
+  string matchingSymbol = "";
+
+  switch (currentSymbol)
+  {
+    case "[":
+      matchingSymbol = "]";
+      break;
+    case "{":
+      matchingSymbol = "}";
+      break;
+    case "(":
+      matchingSymbol = ")";
+      break;
+  }
+
   openingPosition += 1;
-  int closingPosition = message.IndexOf(')');
+
+  closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+
   int length = closingPosition - openingPosition;
   Console.WriteLine(message.Substring(openingPosition, length));
-
-  message = message.Substring(closingPosition + 1);
 }
