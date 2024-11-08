0c2321d0585774892f9d871bd2526a1c0ad371d4
Mahabubur Rahman <microronyy@gmail.com>
2024-11-08 18:47:49 +0600

    modify content first exercise

---
+++ ModifyContent/Program.cs
@@ -0,0 +1,12 @@
+﻿string message = "What is the value <span>between the tags</span>?";
+
+string openSpan = "<span>";
+string closeSpan = "</span>";
+
+int openSpanPosition = message.IndexOf(openSpan);
+int closeSpanPosition = message.IndexOf(closeSpan);
+openSpanPosition += openSpan.Length;
+
+int length = closeSpanPosition - openSpanPosition;
+
+Console.WriteLine(message.Substring(openSpanPosition, length));
