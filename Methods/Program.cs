449eb2c75e8be121ec49d1b1b36b654fd0f37d79
Mahabubur Rahman <microronyy@gmail.com>
2024-11-21 09:59:06 +0600

    methods project

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,63 +1,125 @@
 ﻿using System;
 
-string[] pettingZoo =
-{
-    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
-    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
-    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
-};
+Random random = new Random();
+Console.CursorVisible = false;
+int height = Console.WindowHeight - 1;
+int width = Console.WindowWidth - 5;
+bool shouldExit = false;
 
-PlanSchoolVisit("School A");
-PlanSchoolVisit("School B", 3);
-PlanSchoolVisit("School C", 2);
+// Console position of the player
+int playerX = 0;
+int playerY = 0;
 
-void PlanSchoolVisit(string schoolName, int groups = 6)
+// Console position of the food
+int foodX = 0;
+int foodY = 0;
+
+// Available player and food strings
+string[] states = { "('-')", "(^-^)", "(X_X)" };
+string[] foods = { "@@@@@", "$$$$$", "#####" };
+
+// Current player string displayed in the Console
+string player = states[0];
+
+// Index of the current food
+int food = 0;
+
+InitializeGame();
+
+while (!shouldExit)
 {
-    RandomizeAnimals();
-    string[,] group = AssignGroup(groups);
-    Console.WriteLine(schoolName);
-    PrintGroup(group);
+    if (TerminalResized())
+    {
+        Console.Clear();
+        Console.Write("Console was resized. Program exiting.");
+        shouldExit = true;
+    }
+    Move();
+
 }
 
-void RandomizeAnimals()
+// Returns true if the Terminal was resized 
+bool TerminalResized()
 {
-    Random random = new Random();
-
-    for (int i = 0; i < pettingZoo.Length; i++)
-    {
-        int r = random.Next(pettingZoo.Length);
-
-        string temp = pettingZoo[i];
-        pettingZoo[i] = pettingZoo[r];
-        pettingZoo[r] = temp;
-    }
+    return height != Console.WindowHeight - 1 || width != Console.WindowWidth - 5;
 }
 
-string[,] AssignGroup(int groups = 6)
+// Displays random food at a random location
+void ShowFood()
 {
-    string[,] result = new string[groups, pettingZoo.Length / groups];
-    int start = 0;
+    // Update food to a random index
+    food = random.Next(0, foods.Length);
 
-    for (int i = 0; i < groups; i++)
-    {
-        for (int j = 0; j < result.GetLength(1); j++)
-        {
-            result[i, j] = pettingZoo[start++];
-        }
-    }
+    // Update food position to a random location
+    foodX = random.Next(0, width - player.Length);
+    foodY = random.Next(0, height - 1);
 
-    return result;
+    // Display the food at the location
+    Console.SetCursorPosition(foodX, foodY);
+    Console.Write(foods[food]);
 }
 
-void PrintGroup(string[,] group)
+// Changes the player to match the food consumed
+void ChangePlayer()
 {
-    for (int i = 0; i < group.GetLength(0); i++)
+    player = states[food];
+    Console.SetCursorPosition(playerX, playerY);
+    Console.Write(player);
+}
+
+// Temporarily stops the player from moving
+void FreezePlayer()
+{
+    System.Threading.Thread.Sleep(1000);
+    player = states[0];
+}
+
+// Reads directional input from the Console and moves the player
+void Move()
+{
+    int lastX = playerX;
+    int lastY = playerY;
+
+    switch (Console.ReadKey(true).Key)
     {
-        Console.Write($"Group {i + 1}: ");
-        for (int j = 0; j < group.GetLength(1); j++)
-        {
-            Console.Write($"{group[i, j]} ");
-        }
-        Console.WriteLine();
+        case ConsoleKey.UpArrow:
+            playerY--;
+            break;
+        case ConsoleKey.DownArrow:
+            playerY++;
+            break;
+        case ConsoleKey.LeftArrow:
+            playerX--;
+            break;
+        case ConsoleKey.RightArrow:
+            playerX++;
+            break;
+        case ConsoleKey.Escape:
+            shouldExit = true;
+            break;
     }
+
+    // Clear the characters at the previous position
+    Console.SetCursorPosition(lastX, lastY);
+    for (int i = 0; i < player.Length; i++)
+    {
+        Console.Write(" ");
+    }
+
+    // Keep player position within the bounds of the Terminal window
+    playerX = (playerX < 0) ? 0 : (playerX >= width ? width : playerX);
+    playerY = (playerY < 0) ? 0 : (playerY >= height ? height : playerY);
+
+    // Draw the player at the new location
+    Console.SetCursorPosition(playerX, playerY);
+    Console.Write(player);
+}
+
+// Clears the console, displays the food and player
+void InitializeGame()
+{
+    Console.Clear();
+    ShowFood();
+    Console.SetCursorPosition(0, 0);
+    Console.Write(player);
 }
